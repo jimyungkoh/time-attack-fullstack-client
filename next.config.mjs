8f711b3c4333e6d298bc59e4b21e557634ac34b1
jimyungkoh <kojimyung1206@naver.com>
2024-02-29 11:32:26 +0900

    chore: 라이브러리, 테일윈드, eslint 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "time-attack.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
